b2a3627d813c96eb53e811179d277754d5aad316
“onur”$gitmastergit config --global user.name “onur <onur.denizhan95@gmail.com>
2020-07-17 16:47:16 +0300

    first commit

---
+++ BookStore/Controllers/BookStoreController.cs
@@ -0,0 +1,47 @@
+﻿using BookStore.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BookStore.Controllers
+{
+    public class BookStoreController : ApiController
+    {
+        static Book book = new Book();
+        static List<Book> books = new List<Book>();
+
+        [HttpGet]
+        public List<Book> Get()
+        {
+            return books;
+        }
+
+        [HttpGet]
+        public Book Get(int _id)
+        {
+            return books.Where(x => x.Id == _id).FirstOrDefault();
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Post(Book otherBook)
+        {
+            book.Books.Add(new Book { Author = otherBook.Author, Title = otherBook.Title });
+            // books.Add(new Book { Author = book.Author, Title = book.Title });
+            //books.Add(new Book { Author = book.Author, Title = book.Title, Id = 4 });
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        [HttpPut]
+        public void Put(int id, Book book)
+        {
+        }
+
+        // DELETE: api/BookStore/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public List<Book> Books { get; set; }
+    }
+}
